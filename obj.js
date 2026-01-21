3938f25f108e30ba6b38457ac5e9c020acfa1140
luiz-suf <luizivanpinheiro@outlook.com>
2026-01-21 13:33:52 -0300

    gett/setter

--- obj.js
+++ obj.js
@@ -5,38 +5,21 @@ function produtos(nome, preco, estoque){
 
     Object.defineProperty(this, 'estoque', {
         enumerble: true,
-        value : estoque,
-        writable: true,
-        configurable: false
+        configurable: true,
+        get: function(){
+            return estoque;
+        },
+        set: function(valor){
+            if (typeof valor !== 'number'){
+                throw new TypeError('valor invalido')
+            }
+        }
+
     })
     
-     Object.defineProperties(this,{ 
-        nome : {
-            enumerable: true,
-            value: nome,
-            writable: true,
-            configurable: false
-        },
-        preco:{
-            enumerable: true,
-            value: preco,
-            writable: true,
-            configurable: false
-        },
-        estoque: {
-            enumerable: true,
-            value: estoque,
-            writable: true,
-            configurable: false
-        }
-     })
-
 
 }
 const p1 = new produtos('camiseta', 20, 30);
 
-//delete p1.estoque;
-console.log(Object.keys(p1));
-for(let chave in p1){
-    console.log(chave)
-}
+p1.estoque = 40
+console.log(p1.estoque);
